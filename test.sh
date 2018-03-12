0ef8f256227f0c0a977789352163fbf0a2d934b8
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-03-12 12:08:54 +0100

    Update depedendency version acacha/llum

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 composer install --prefer-source --no-interaction --dev
+composer global require "laravel/installer"
+composer global require "acacha/llum"
 ./sandbox_setup.sh
 cd sandbox
 ./vendor/phpunit/phpunit --configuration ../phpunit.xml ../tests/InstallCommandTest.php
