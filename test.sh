33f53b8dc5734887380d02540f6aa35130b0d9f0
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-03-12 12:15:24 +0100

    Sync

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 composer install --prefer-source --no-interaction --dev
 composer global require "laravel/installer"
 composer global require "acacha/llum"
+composer global require "acacha/adminlte-laravel-installer"
 ./sandbox_setup.sh
 cd sandbox
 ./vendor/phpunit/phpunit --configuration ../phpunit.xml ../tests/InstallCommandTest.php
